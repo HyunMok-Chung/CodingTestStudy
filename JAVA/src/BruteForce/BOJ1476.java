4db6fb5e8cbe6c10962c6eea56905fadb76737b7
HyunMok-Chung <1021hm@naver.com>
2023-02-08 18:41:45 +0900

    23.02.08

---
+++ JAVA/src/BruteForce/BOJ1476.java
@@ -0,0 +1,33 @@
+package BruteForce;
+
+import java.util.*;
+import java.io.*;
+
+public class BOJ1476 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int e = Integer.parseInt(st.nextToken());
+        int s = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int i=1;
+        int E=1;
+        int S=1;
+        int M=1;
+        while(true){
+            if(E>15)
+                E=1;
+            if(S>28)
+                S=1;
+            if(M>19)
+                M=1;
+            if(E==e && S==s & M==m)
+                break;
+            i++;
+            E++;
+            S++;
+            M++;
+        }
+        System.out.println(i);
+    }
+}
