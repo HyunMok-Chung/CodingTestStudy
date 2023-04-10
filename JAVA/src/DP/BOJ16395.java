125d861a48367e4b39de24a01c125a7811a0a755
HyunMok-Chung <1021hm@naver.com>
2023-04-10 19:12:56 +0900

    BOJ16395 코드 수정

--- JAVA/src/DP/BOJ16395.java
+++ JAVA/src/DP/BOJ16395.java
@@ -12,7 +12,7 @@ public class BOJ16395 {  // 파스칼의 삼각형
         int[][] dp = new int [n+1][n+1];
         dp[1][1] = 1;
         for(int i=2;i<n+1;i++){
-            for(int j=1;j<n+1;j++){
+            for(int j=1;j<=i;j++){
                 dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
             }
         }
